f9c3587b80eed7a7f03ff3bc1401221bbcdb82b1
Brandon <157135813+brandonangell30@users.noreply.github.com>
2025-02-27 12:44:04 -0500

    Add files via upload

---
+++ esc_char/EscapeCharacterHW.java
@@ -0,0 +1,19 @@
+package escapecharacterhw;
+
+/*
+ * Name: Brandon Angell
+ * Date: 8/23/22
+ * Purpose: Print ASCII art and print Isaac Asimov quote.
+ */
+
+public class EscapeCharacterHW 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.print(" [@@]               [00]\n/|__|\\             /|##|\\\n d  b               d  b\n\n");
+		System.out.print("\"I don\'t fear computers.\n I fear the lack of them.\"\n     - Isaac Asimov");
+
+	}
+
+}
