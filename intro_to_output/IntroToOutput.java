9c369970f2f1c02e150544927f62df252e1450ce
Brandon <157135813+brandonangell30@users.noreply.github.com>
2025-02-27 12:39:48 -0500

    Add files via upload

---
+++ intro_to_output/IntroToOutput.java
@@ -0,0 +1,19 @@
+package introtooutput;
+
+public class IntroToOutput 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("                                                               Brandon Angell      ");
+		System.out.println("");
+		System.out.println("Brandon Angell");
+		System.out.println("Brandon Angell");
+		System.out.println("Brandon Angell");
+		System.out.println("");
+		System.out.println("********");
+		System.out.println("**java**");
+		System.out.print("********");
+	}
+
+}
