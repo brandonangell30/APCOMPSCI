4abd8da4e98978bd5a092df9c2b695872c68e9ff
Brandon <157135813+brandonangell30@users.noreply.github.com>
2025-02-27 12:17:36 -0500

    Add files via upload

---
+++ 2esc_tasks/TwoEscapeTasks.java
@@ -0,0 +1,22 @@
+package twoescapetasks;
+
+/*
+ * Name: Brandon Angell
+ * Date: 8/23/22
+ * Purpose: Print JAVA diagonally and display a house.
+ */
+
+public class TwoEscapeTasks 
+{
+
+	public static void main(String[] args) 
+	{
+		//This prints JAVA diagonally
+		System.out.println("J\n\tA\n\t\tV\n\t\t\tA\n\n    &\n    _");
+		//This builds a house :)
+		System.out.println(" /\\| |\n/--\\ |\n|__|_|");
+		
+		
+	}  
+
+}
